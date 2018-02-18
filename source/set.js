7bbb5c2dfaa36d4efc118a9754b1e994a66d67ea
Unknown <dariago12@gmail.com>
2018-02-18 23:38:20 +0300

    minor changes

--- source/set.js
+++ source/set.js
@@ -3,12 +3,12 @@
 // рекурсивная функция, отбрасывает точку - спускается на уровень вложенности
 function set(object, property, value) {
     let prop;
-    if(typeof(property) === 'string') {
+    if(typeof property === 'string') {
         prop = property.split('.').slice(1);
     } else
         prop = property;
     
-    if(prop.length == 1) {
+    if(prop.length === 1) {
         object[prop[0]] = value;
         return object;
     }
@@ -16,8 +16,9 @@ function set(object, property, value) {
     const currProp = prop[0];
     prop.shift();
 
-    if(!(currProp in object)) // у объекта нет требуемого свойства
+    if(!(currProp in object)) {// у объекта нет требуемого свойства
     	object[currProp] = {};
+    }
 
     object[currProp] = set(object[currProp], prop, value);
     return object;
